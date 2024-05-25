38dbcf458743f531a3a0a0df44d43e5c1691a916
scarf <greenscarf005@gmail.com>
2024-05-25 14:01:50 +0900

    refactor: simplify CSS

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Helmet, HelmetProvider } from "react-helmet-async"
 import { isoDate } from "./isoDate.ts"
 import { clockHours } from "./ClockIcon.tsx"
 import { useDayjs } from "./useDayjs.tsx"
-import "./App.css"
 
 export const App = () => {
   const dayjs = useDayjs()
@@ -15,7 +14,7 @@ export const App = () => {
         <title>{time}</title>
         <link rel="icon" type="image/svg+xml" href={clockHours(dayjs.hour())} />
       </Helmet>
-      <main className="App">
+      <main>
         <h1>{date}</h1>
         <h2>{datetime}</h2>
         <h3>{week}</h3>
